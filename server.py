12253b7982b1e52f844c969f27faa927618f9f61
Michael Moorley <themoorleygroup@gmail.com>
2021-10-28 09:06:33 -0400

    Initial Commit - Logic For Menu Completed

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from _datetime import datetime
+from menu import starterMenu, traditionalMenu, soup_salad, pho_noodle, curry, main_course, noodle_lover
+from menu import vegetarian, side_dishes, hot_drinks, cold_drinks, lunch_menu
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    current_year = datetime.now().year
+    return render_template("index.html", year=current_year)
+
+
+@app.route("/Menu")
+def menu():
+    current_year = datetime.now().year
+    s_menu = starterMenu
+    t_menu = traditionalMenu
+    ss_menu = soup_salad
+    pho_menu = pho_noodle
+    c_menu = curry
+    m_menu = main_course
+    n_menu = noodle_lover
+    v_menu = vegetarian
+    si_menu = side_dishes
+    h_drinks = hot_drinks
+    c_drinks = cold_drinks
+    l_menu = lunch_menu
+    return render_template("menu.html", year=current_year, s_items=s_menu, t_items=t_menu, ss_items=ss_menu,
+                           pho_items=pho_menu, c_items=c_menu, m_items=m_menu, n_items=n_menu, v_items=v_menu,
+                           si_items=si_menu, h_items=h_drinks, co_items=c_drinks, l_items=l_menu)
+
+
+@app.route("/Contact")
+def contact():
+    current_year = datetime.now().year
+    return render_template("contact.html", year=current_year)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/javascript/menu.js
@@ -0,0 +1,108 @@
+
+function lunch() {
+    var x = document.getElementById('lunch_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function starter() {
+    var x = document.getElementById('starter_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function traditional() {
+    var x = document.getElementById('traditional_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function soup() {
+    var x = document.getElementById('soup_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function pho() {
+    var x = document.getElementById('pho_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function curry() {
+    var x = document.getElementById('curry_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function main() {
+    var x = document.getElementById('main_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function noodle() {
+    var x = document.getElementById('noodle_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function veg() {
+    var x = document.getElementById('veg_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function side() {
+    var x = document.getElementById('side_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function hot() {
+    var x = document.getElementById('hot_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function cold() {
+    var x = document.getElementById('cold_button');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
